86d79ca513bfe2a9f901019595b12639d29d6d98
cariza <cariza@asesoftware.com>
2023-12-14 21:26:10 -0500

    se puede registrar un evento y se muestran las horas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
-
+import { Component, signal, ChangeDetectorRef } from '@angular/core';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import rrulePlugin from '@fullcalendar/rrule'
+import listPlugin from '@fullcalendar/list'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calendar';
+  calendarVisible = signal(true);
+  calendarOptions = signal<CalendarOptions>({
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      // rrulePlugin,
+      // listPlugin
+    ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,week,dayGridDay',
+    },
+    views: {
+      week: {
+        type: 'timeGridWeek',
+        duration: { days: 7 },
+        buttonText: 'Semana',
+        slotMinTime: '00:00:00',
+        timeHint: '03:00:00',
+        slotLabelFormat: {
+          hour: 'numeric',
+          minute: '2-digit',
+          hour12: true
+        },
+        allDaySlot: false,
+        nowIndicator: true,
+      }
+    },
+    allDayText: 'Todo el día',
+
+    initialView: 'week',
+    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+
+    displayEventTime: true,
+    displayEventEnd: true,
+    height: 650,
+    eventTimeFormat: {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    },
+    locale: 'es',
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this)
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  });
+  currentEvents = signal<EventApi[]>([]);
+
+  constructor(private changeDetector: ChangeDetectorRef) {
+  }
+
+  handleCalendarToggle() {
+    this.calendarVisible.update((bool) => !bool);
+  }
+
+  handleWeekendsToggle() {
+    this.calendarOptions.mutate((options) => {
+      options.weekends = !options.weekends;
+    });
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+    console.log(selectInfo)
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  prueba() {
+    let selectInfo: DateSelectArg;
+    // const calendarApi = selectInfo.view.calendar;
+    console.log(this.calendarOptions())
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents.set(events);
+    this.changeDetector.detectChanges(); // workaround for pressionChangedAfterItHasBeenCheckedError
+  }
 }
